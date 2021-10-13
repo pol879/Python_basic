2d5af49fa7798bfcaaf1391a4df0f5585c074ca0
Pol <gorshenina.pa@phystech.edu>
2021-10-14 02:25:30 +0300

    my first commit

---
+++ lab3_zad3.py
@@ -0,0 +1,31 @@
+import turtle as tu
+
+v_ox = 40
+v_oy = 70
+tu.shape('circle')
+tu.speed(10)
+tu.pensize(3)
+tu.penup()
+tu.goto(-600, 0)
+tu.pendown()
+chi = 9.8/2
+
+
+def parabola(v_ox, v_oy):
+    t = 0
+    i = (1 / chi) * v_oy
+    x_o = tu.xcor()
+
+    while t < i:
+        x = x_o + v_ox * t
+        y = v_oy * t - chi * t ** 2
+        tu.goto(x, y)
+        t += 0.05
+
+
+for i in range(5):
+    parabola(v_ox, v_oy)
+    v_oy /= 1.3
+    v_ox /= 1.7
+
+tu.exitonclick()
